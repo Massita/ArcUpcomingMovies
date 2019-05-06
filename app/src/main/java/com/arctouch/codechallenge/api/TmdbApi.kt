f9b41c87f46aa1dae50107d889762c33e89a878d
Tiago Massita <s2it_tfonseca@uolinc.com>
2019-05-06 17:42:56 -0300

    Creating an api builder and getting rid of the BaseActivity

--- app/src/main/java/com/arctouch/codechallenge/api/TmdbApi.kt
+++ app/src/main/java/com/arctouch/codechallenge/api/TmdbApi.kt
@@ -4,6 +4,10 @@ import com.arctouch.codechallenge.model.GenreResponse
 import com.arctouch.codechallenge.model.Movie
 import com.arctouch.codechallenge.model.UpcomingMoviesResponse
 import io.reactivex.Observable
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -15,6 +19,14 @@ interface TmdbApi {
         const val API_KEY = "1f54bd990f1cdfb230adb312546d765d"
         const val DEFAULT_LANGUAGE = "pt-BR"
         const val DEFAULT_REGION = "BR"
+
+        fun create() : TmdbApi = Retrofit.Builder()
+                .baseUrl(TmdbApi.URL)
+                .client(OkHttpClient.Builder().build())
+                .addConverterFactory(MoshiConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+                .create(TmdbApi::class.java)
     }
 
     @GET("genre/movie/list")
